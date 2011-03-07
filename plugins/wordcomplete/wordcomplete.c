535df5a293ce3b4995618ba3da599bc5484b05a9
Kevin Porter <kportertx@gmail.com>
2011-03-07 05:47:40 -0800

    Cleaned up wordcomplete a bit.  Still messy but now there is only one exit point.

--- plugins/wordcomplete/wordcomplete.c
+++ plugins/wordcomplete/wordcomplete.c
@@ -45,6 +45,7 @@ void restore(int s) {
   die=1;
 }
 
+/*
 void handleKey(char* keyname, char* word, char* wp){
   if(keyname == clear){
     wp = word;
@@ -62,8 +63,9 @@ void handleKey(char* keyname, char* word, char* wp){
     *wp = '\0';
   }
   //printf("%s, %x, %x\n", keyname, word, wp);
-  printf("%s\n", word);
+  //printf("%s\n", word);
 }
+*/
 
 int main(int argc, char* argv) {
   unsigned char ok[32];
@@ -100,160 +102,178 @@ int main(int argc, char* argv) {
         if(nk[i] != 0) {
           //pressed key
           keycode=i*8+fooling(nk[i] ^ ok[i]);
-          char ks = XKeycodeToKeysym(display, keycode, 0);
-          char *keyname = XKeysymToString(ks);
-          if(keycode == 65){  // space
+          char ks;
+          char *keyname;
+          keyname="";
+          switch(keycode){
+            // numpad
+          case 79: // 7
+          case 80: // 8
+          case 81: // 9
+          case 83: // 4
+          case 84: // 5
+          case 85: // 6
+          case 87: // 1
+          case 88: // 2
+          case 89: // 3
+          case 90: // 0
+            // Numpad keys do not work att
             keyname = clear;
+            break;
+            // numbers
+          case 10: // 1
+          case 11: // 2
+          case 12: // 3
+          case 13: // 4
+          case 14: // 5
+          case 15: // 6
+          case 16: // 7
+          case 17: // 8
+          case 18: // 9
+          case 19: // 0
+            // top row
+          case 24: // q
+          case 25: // w
+          case 26: // e
+          case 27: // r
+          case 28: // t
+          case 29: // y
+          case 30: // u
+          case 31: // i
+          case 32: // o
+          case 33: // p
+            // middle row
+          case 38: // a
+          case 39: // s
+          case 40: // d
+          case 41: // f
+          case 42: // g
+          case 43: // h
+          case 44: // j
+          case 45: // k
+          case 46: // l
+            // bottom row
+          case 52: // z
+          case 53: // x
+          case 54: // c
+          case 55: // v
+          case 56: // b
+          case 57: // n
+          case 58: // m
+            ks = XKeycodeToKeysym(display, keycode, 0);
+            keyname = XKeysymToString(ks);
+            break;
+            
+          case 105: // "ctrl down";
+          case 133: // "win down";
+          case 108: // "alt gr down";
+          case 64: // "alt down";
+          case 34: // "å";
+          case 48: // "ä";
+          case 47: // "ö";
+          case 23: // "tab";
+          case 67: // "F1";
+          case 68: // "F2";
+          case 69: // "F3";
+          case 70: // "F4";
+          case 71: // "F5";
+          case 72: // "F6";
+          case 73: // "F7";
+          case 74: // "F8";
+          case 75: // "F9";
+          case 76: // "F10";
+          case 95: // "F11";
+          case 96: // "F12";
+          case 9:  // "escape";
+          case 111: // "up"
+          case 113: // "left"
+          case 114: // "right"
+          case 116: // "down"
+          case 65: // "space"
+          case 35: // "]"
+          case 51: // "\"
+          case 59: // ","
+          case 60: // "."`
+          case 61: // "/"
+          case 49: // "~"
+          case 20: // "-"
+          case 21: // "="
+          case 119: // "del" 
+          case 36:  // "return";
+          case 104: // "return";
+            keyname = clear;
+            break;
+          case 22: // "backspace";
+            keyname = bkspce;
+            break;
+          case 50:
+          case 62: // "shift down";
+          case 66: // "caps lock";
+          case 37: // "control"
+          case 135: // "menu"
+          case 107: // "PrtSc"
+          case 127: // "pause"
+          case 118: // "insert"
+          case 77: // "Num Lock"
+          case 78: // "Scroll Lock"
+            keyname = ignore;
+            break;
+          default:
+            printf("\n*Error: keycode %d not recognized*\n",keycode);
           }
-          if(!keyname) {
-            keyname="";
-            switch(keycode){
-            case 37:
-            case 105:
-              //keyname="ctrl down";
-            case 133:
-              //keyname="win down";
-            case 108:
-              //keyname="alt gr down";
-            case 64:
-              //keyname="alt down";
-            case 34:
-              //keyname="å";
-            case 48:
-              //keyname="ä";
-            case 47:
-              //keyname="ö";
-            case 23:
-              //keyname="tab";
-            case 67:
-              //keyname="F1";
-            case 68:
-              //keyname="F2";
-            case 69:
-              //keyname="F3";
-            case 70:
-              //keyname="F4";
-            case 71:
-              //keyname="F5";
-            case 72:
-              //keyname="F6";
-            case 73:
-              //keyname="F7";
-            case 74:
-              //keyname="F8";
-            case 75:
-              //keyname="F9";
-            case 76:
-              //keyname="F10";
-            case 95:
-              //keyname="F11";
-            case 96:
-              //keyname="F12";
-            case 9:
-              //keyname="escape";
-            case 32:
-              //keyname="space";
-              printf("space");
-            case 36:
-            case 104:
-              //keyname="return";
-              keyname = clear;
-              break;
-            case 22:
-              //keyname="backspace";
-              keyname = bkspce;
-              break;
-            case 50:
-            case 62:
-              //keyname="shift down";
-            case 66:
-              //keyname="caps lock";
-              keyname = ignore;
-              break;
-            }
-            if(keyname == clear){
-              wp = word;
-              *wp = '\0';
-            } else if(keyname == bkspce){
-              if(wp != &word[0]){
-                wp--;
-                *wp = '\0';
-              }
-            } else if(keyname == ignore){
-              //nothing
-            } else {
-              *wp = *keyname;
-              wp++;
-              *wp = '\0';
-            }
-            //printf("%s, %x, %x\n", keyname, word, wp);
-            printf("%s\n", word);
-            //handleKey(keyname,&word[0],wp);
-            /*
-            if(strcmp("",keyname)==0) {
-              printf("[unknown: %u]",keycode);
-            } else {
-              printf("[%s]",keyname);
-            }
-            */
-          } else {
-            if(strcmp(keyname,"Q")==0) {
-              //keyname="[left key]";
-              keyname = clear;
-            } else if(strcmp(keyname,"S")==0){
-              //keyname="[right key]";
-              keyname = clear;
-            } else if(strcmp(keyname,"R")==0){
-              //keyname="[up key]";
-              keyname = clear;
-            } else if(strcmp(keyname,"T")==0){
-              //keyname="[down key]";
-              keyname = clear;
-            } 
-            //printf("%s",keyname);
-            if(keyname == clear){
-              wp = word;
-              *wp = '\0';
-            } else if(keyname == bkspce){
-              if(wp != &word[0]){
-                wp--;
-                *wp = '\0';
-              }
-            } else if(keyname == ignore){
-              //nothing
-            } else {
-              *wp = *keyname;
-              wp++;
-              *wp = '\0';
-            }
-            //printf("%s, %x, %x\n", keyname, word, wp);
-            printf("%s\n", word);
 
-            //handleKey(keyname,&word[0],wp);
-          }
+          if(strcmp(keyname,"Q")==0) {
+            //keyname="[left key]";
+            keyname = clear;
+          } else if(strcmp(keyname,"S")==0){
+            //keyname="[right key]";
+            keyname = clear;
+          } else if(strcmp(keyname,"R")==0){
+            //keyname="[up key]";
+            keyname = clear;
+          } else if(strcmp(keyname,"T")==0){
+            //keyname="[down key]";
+            keyname = clear;
+          } 
+
+          if(keyname == clear){
+            wp = word;
+            *wp = '\0';
+          } else if(keyname == bkspce){
+            if(wp != &word[0]){
+              wp--;
+              *wp = '\0';
+            }
+          } else if(keyname == ignore){
+            //nothing
+          } else {
+            *wp = *keyname;
+            wp++;
+            *wp = '\0';
+          }            //printf("%s",keyname);
         } else {
-            keycode=i*8+fooling(nk[i] ^ ok[i]);
-            /*
+          keycode=i*8+fooling(nk[i] ^ ok[i]);
+          /*
             switch(keycode){
             case 50:
             case 62:
-              printf("[shift up]");
-              break;
+            printf("[shift up]");
+            break;
             case 37:
             case 105:
-              printf("[ctrl up]");
-              break;
+            printf("[ctrl up]");
+            break;
             case 133:
-              printf("[win up]");
-              break;
+            printf("[win up]");
+            break;
             case 108:
-              printf("[alt gr up]");
-              break;
+            printf("[alt gr up]");
+            break;
             case 64:
-              printf("[alt up]");
-              break;
+            printf("[alt up]");
+            break;
             }
-            */
+          */
         }
         ok[i]=nk[i];
       }
